831cfa5bdf43ef775ea158bcdd3e7b371d0ade45
Ramki-92 <ksramki92@gmail.com>
2024-09-03 18:09:57 +0530

    Updated upload filename

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,24 +1,18 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 import os
 
 # Path to the service account credentials file
-# credentials_path = '/tmp/service-account-credentials.json'
-
 credentials_path = os.getenv('SERVICE_ACCOUNT_FILE')
 
-# Authenticate and create the PyDrive client
+# Authenticate and create the Google Drive client
 credentials = service_account.Credentials.from_service_account_file(
     credentials_path,
     scopes=['https://www.googleapis.com/auth/drive.file']
 )
 
-gauth = GoogleAuth()
-gauth.credentials = credentials
-
-# Create a Google Drive client
-drive = GoogleDrive(gauth)
+drive_service = build('drive', 'v3', credentials=credentials)
 
 # Path to the CSV file in Jenkins workspace
 file_path = 'kpn_fresh_items_20240903_104847.csv'
@@ -26,13 +20,20 @@ file_path = 'kpn_fresh_items_20240903_104847.csv'
 # Folder ID of the Google Drive folder where you want to upload the file
 folder_id = '1r3uqaGVRtLqf2r2s0d27K6hujTWSrkM4'  # Replace with your actual folder ID
 
-# Create a file object in Google Drive, specifying the folder ID
-gfile = drive.CreateFile({'title': os.path.basename(file_path), 'parents': [{'id': folder_id}]})
+# Create a file metadata dictionary
+file_metadata = {
+    'name': os.path.basename(file_path),
+    'parents': [folder_id]
+}
 
-# Set the content of the file from the local file
-gfile.SetContentFile(file_path)
+# Create a media file upload object
+media = MediaFileUpload(file_path)
 
 # Upload the file
-gfile.Upload()
+file = drive_service.files().create(
+    body=file_metadata,
+    media_body=media,
+    fields='id'
+).execute()
 
-print(f"File {os.path.basename(file_path)} uploaded to Google Drive folder with ID {folder_id} successfully.")
+print(f"File {os.path.basename(file_path)} uploaded to Google Drive folder with ID {folder_id} successfully. File ID: {file.get('id')}")
